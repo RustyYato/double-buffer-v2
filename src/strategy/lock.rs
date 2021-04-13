c917c7bb202f1489cff1c52f430f036da0adb97f
RustyYato <krishna.sd.2012@gmail.com>
2021-04-13 18:05:25 -0400

    prevent readers from starving swaps

--- src/strategy/lock.rs
+++ src/strategy/lock.rs
@@ -1,15 +1,23 @@
 use core::cell::Cell;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use crate::traits::Strategy;
-use parking_lot::{lock_api::RawRwLock as _, RawRwLock};
+use parking_lot::{
+    lock_api::{RawRwLock as _, RawRwLockFair, RawRwLockRecursive},
+    RawRwLock,
+};
 
 pub struct LockStrategy {
+    wait_to_swap: AtomicBool,
     lock: RawRwLock,
 }
 
 #[allow(clippy::declare_interior_mutable_const)]
 impl LockStrategy {
-    pub const INIT: Self = Self { lock: RawRwLock::INIT };
+    pub const INIT: Self = Self {
+        lock: RawRwLock::INIT,
+        wait_to_swap: AtomicBool::new(false),
+    };
 }
 
 impl Default for LockStrategy {
@@ -42,19 +50,27 @@ unsafe impl Strategy for LockStrategy {
     }
 
     fn finish_capture_readers(&self, _: &mut Self::WriterTag, _: Self::FastCapture) -> Self::Capture {
+        self.wait_to_swap.store(true, Ordering::Release);
         self.lock.lock_exclusive();
         unsafe {
             self.lock.unlock_exclusive();
         }
+        self.wait_to_swap.store(false, Ordering::Release);
         Capture(())
     }
 
     fn readers_have_exited(&self, _: &mut Self::Capture) -> bool { true }
 
     fn begin_guard(&self, _: &mut Self::ReaderTag) -> Self::RawGuard {
-        self.lock.lock_shared();
+        self.lock.lock_shared_recursive();
         RawGuard(())
     }
 
-    unsafe fn end_guard(&self, _: Self::RawGuard) { self.lock.unlock_shared(); }
+    unsafe fn end_guard(&self, _: Self::RawGuard) {
+        if self.wait_to_swap.load(Ordering::Acquire) {
+            self.lock.unlock_shared_fair();
+        } else {
+            self.lock.unlock_shared();
+        }
+    }
 }
