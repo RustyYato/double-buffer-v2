57d3a536af1f5d50ecd6bb638ee44f065e817d4e
RustyYato <krishna.sd.2012@gmail.com>
2021-05-16 15:55:47 -0400

    make `OpList::apply` panic safe

--- src/op_list.rs
+++ src/op_list.rs
@@ -3,6 +3,8 @@ use std::vec::Vec;
 
 use crate::traits::Operation;
 
+const POISON_BIT: usize = (!0) ^ (!0 >> 1);
+
 pub struct OpList<O> {
     operations: Vec<O>,
     applied: usize,
@@ -22,16 +24,70 @@ impl<O> OpList<O> {
     where
         O: Operation<B>,
     {
-        let applied = core::mem::take(&mut self.applied);
-        
-        for operation in self.operations.drain(..applied) {
-            operation.apply_final(buffer);
+        struct SetOnDrop<'a>(&'a mut usize, usize);
+
+        impl Drop for SetOnDrop<'_> {
+            fn drop(&mut self) { *self.0 = self.1 }
         }
 
-        self.applied = self.operations.len();
+        if self.applied & POISON_BIT == 0 {
+            self.apply_final(buffer);
+        }
+
+        self.applied |= POISON_BIT;
+        let mut set_on_drop = SetOnDrop(&mut self.applied, POISON_BIT);
+        let applied = &mut set_on_drop.1;
 
         for operation in self.operations.iter_mut() {
+            *applied += 1;
             operation.apply(buffer);
+        }
+
+        drop(set_on_drop);
+        self.assume_no_panic();
+    }
+
+    pub fn assume_no_panic(&mut self) { self.applied &= !POISON_BIT; }
+
+    #[inline(always)]
+    fn apply_final<B: ?Sized>(&mut self, buffer: &mut B)
+    where
+        O: Operation<B>,
+    {
+        struct Fixer<'a, T> {
+            begin: *mut T,
+            curr: *mut T,
+            len: usize,
+            vec: &'a mut Vec<T>,
+        }
+
+        impl<T> Drop for Fixer<'_, T> {
+            fn drop(&mut self) {
+                unsafe {
+                    self.begin.copy_from(self.curr, self.len);
+                    self.vec.set_len(self.len);
+                }
+            }
+        }
+
+        let len = self.operations.len();
+        let ptr = self.operations.as_mut_ptr();
+
+        let mut fixer = Fixer {
+            begin: ptr,
+            curr: ptr,
+            len,
+            vec: &mut self.operations,
+        };
+
+        while self.applied != 0 {
+            unsafe {
+                self.applied -= 1;
+                fixer.len -= 1;
+                let ptr = fixer.curr;
+                fixer.curr = fixer.curr.add(1);
+                ptr.read().apply_final(buffer)
+            }
         }
     }
 
